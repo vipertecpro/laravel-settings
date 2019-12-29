2a8fddad57878ab555dc7100160b861364f20c07
Vipul Walia <vipertecpro@gmail.com>
2019-12-29 08:44:01 +0530

    File URL Updated

--- src/resources/views/settings/types_value/file.blade.php
+++ src/resources/views/settings/types_value/file.blade.php
@@ -4,7 +4,7 @@
         <input class="form-control" name="value" id="value" type="file" placeholder="Value"/>
         @if(!empty($setting->value))
             <a style="margin-bottom: 5px;" class="btn btn-info"
-               href="{{ url(config('settings.route').'/download/'.$setting->id) }}"
+               href="{{ url(config('settings.route_prefix').'/download/'.$setting->id) }}"
                target="_blank">
                 <i class="fa fa-download"></i> Download {{ $setting->getOriginal('value') }}
             </a>
